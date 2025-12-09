1c4b896511eb63f63518304710d7a227a9290191
Alexander Carter <alrunner4@gmail.com>
2025-12-09 15:11:34 -0800

    package metadata and repl

--- flake.nix
+++ flake.nix
@@ -7,19 +7,38 @@
 
   outputs = { self, nixpkgs }:
     let
+        nixpkgslib = import <nixpkgs/lib>;
+        transitive-dependencies = p: nixpkgslib.unique
+          (p.idrisLibraries ++ builtins.concatMap transitive-dependencies p.idrisLibraries);
+        decorate-package = pkgs: p: p // rec {
+          idrisLibrariesClosure = transitive-dependencies p;
+          repl = pkgs.writeShellScript "${p.ipkgName}-repl" ''
+            export IDRIS2_PACKAGE_PATH+=:${
+              builtins.concatStringsSep ":"
+                (builtins.map
+                  (dep: "${dep.library {}}/lib/idris2-${pkgs.idris2.version}")
+                  idrisLibrariesClosure)}
+            exec ${pkgs.rlwrap}/bin/rlwrap --ansi-colour-aware --no-children \
+                ${pkgs.idris2}/bin/idris2 --repl ${p.ipkgName}.ipkg
+            '';
+        };
         pkgs = import nixpkgs { system = "x86_64-linux"; };
         lib = {
           inherit pkgs;
           importFromGitHub = {owner, repo, rev, hash ? ""}:
-              import (pkgs.fetchFromGitHub { inherit owner repo rev hash; })
-                  { idrx = self; };
-          importFromSrc = pkgs.idris2Packages.buildIdris;
+            let pkg = import (pkgs.fetchFromGitHub { inherit owner repo rev hash; })
+              { idrx = self; };
+            in decorate-package pkgs pkg;
+          importFromSrc = {src, ipkgName, idrisLibraries, version ? ""}:
+            decorate-package pkgs
+              (pkgs.idris2Packages.buildIdris {inherit src ipkgName idrisLibraries version; }
+                // { inherit ipkgName idrisLibraries version; });
           upstream = pkgs.idris2Packages;
         };
     in
     {
       packages.x86_64-linux = {
-        default = pkgs.hello // lib;
+        default = pkgs.hello;
         inherit lib;
       };
     } // lib;

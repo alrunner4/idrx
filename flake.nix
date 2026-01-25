779ad6e9b4d79c6abe605d6a711ba7d86eeb7e51
Alexander Carter <alrunner4@gmail.com>
2026-01-24 17:43:33 -0800

    try IDRIS2_CPPFLAGS for repl

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
                 (builtins.map
                   (dep: "${dep.library {}}/lib/idris2-${pkgs.idris2.version}")
                   idrisLibrariesClosure)}
-            export IDRIS2_CFLAGS+=" -I${builtins.concatStringsSep " -I" (builtins.map (i: "${i}") p.buildInputs)}/include"
+            export IDRIS2_CPPFLAGS+=" -I${builtins.concatStringsSep " -I" (builtins.map (i: "${i}") p.buildInputs)}/include"
             exec ${pkgs.rlwrap}/bin/rlwrap --ansi-colour-aware --no-children \
                 ${pkgs.idris2}/bin/idris2 --repl ${p.ipkgName}.ipkg
             '';

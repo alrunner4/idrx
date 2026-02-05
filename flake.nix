159636e6855bae93bcfbf7b0cb7c6b1af2da7e1d
Alexander Carter <alrunner4@gmail.com>
2026-02-04 21:38:03 -0800

    have importFromGitHub and importFromSrc produce flake-style output

--- flake.nix
+++ flake.nix
@@ -7,49 +7,52 @@
 
   outputs = { self, nixpkgs }:
     let
-        transitive-dependencies = p: nixpkgs.lib.unique
-          (p.idrisLibraries ++ builtins.concatMap transitive-dependencies p.idrisLibraries);
-        decorate-package = pkgs: p: p // rec {
-          idrisLibrariesClosure = transitive-dependencies p;
-          repl = pkgs.writeShellScriptBin "${p.ipkgName}-repl" ''
-            export IDRIS2_PACKAGE_PATH=${
-              builtins.concatStringsSep ":"
-                (builtins.map
-                  (dep: "${dep.library {}}/lib/idris2-${pkgs.idris2.version}")
-                  idrisLibrariesClosure)}
-            export CPPFLAGS="${
-              builtins.concatStringsSep " "
-                (builtins.map (i: "-I${i}/include") p.buildInputs)}"
-            LIBPATH+=":${
-              builtins.concatStringsSep ":"
-                (builtins.map (l: "${l}/lib") p.runtimeInputs)}"
-            export LIBRARY_PATH=$LIBPATH
-            export LD_LIBRARY_PATH=$LIBPATH
-            exec ${pkgs.rlwrap}/bin/rlwrap --ansi-colour-aware --no-children \
-                ${pkgs.idris2}/bin/idris2 --repl ${p.ipkgName}.ipkg
-            '';
-        };
-        lib = pkgs: {
-          importFromGitHub = {owner, repo, rev, hash ? ""}:
-            let pkg = import (pkgs.fetchFromGitHub { inherit owner repo rev hash; })
-              { idrx = self; };
-            in decorate-package pkgs pkg;
-          importFromSrc = {src, ipkgName, idrisLibraries ? [], version ? "", buildInputs ? [], runtimeInputs ? []}:
-            decorate-package pkgs
-              (pkgs.idris2Packages.buildIdris {
-                inherit src ipkgName idrisLibraries version;
-                nativeBuildInputs = buildInputs;
-                buildInputs = runtimeInputs;
-              } // {
-                inherit buildInputs runtimeInputs ipkgName idrisLibraries version;
-              });
-          upstream = pkgs.idris2Packages;
-        };
+      transitive-dependencies = p: nixpkgs.lib.unique
+        (p.idrisLibraries ++ builtins.concatMap transitive-dependencies p.idrisLibraries);
+      decorate-package = pkgs: p: p // rec {
+        idrisLibrariesClosure = transitive-dependencies p;
+        repl = pkgs.writeShellScriptBin "${p.ipkgName}-repl" ''
+          export IDRIS2_PACKAGE_PATH=${
+            builtins.concatStringsSep ":"
+              (builtins.map
+                (dep: "${dep.library {}}/lib/idris2-${pkgs.idris2.version}")
+                idrisLibrariesClosure)}
+          export CPPFLAGS="${
+            builtins.concatStringsSep " "
+              (builtins.map (i: "-I${i}/include") p.buildInputs)}"
+          LIBPATH+=":${
+            builtins.concatStringsSep ":"
+              (builtins.map (l: "${l}/lib") p.runtimeInputs)}"
+          export LIBRARY_PATH=$LIBPATH
+          export LD_LIBRARY_PATH=$LIBPATH
+          exec ${pkgs.rlwrap}/bin/rlwrap --ansi-colour-aware --no-children \
+              ${pkgs.idris2}/bin/idris2 --repl ${p.ipkgName}.ipkg
+          '';
+      };
     in
     {
       systems = builtins.attrNames nixpkgs.outputs.legacyPackages;
       packages = builtins.mapAttrs
         (system: pkgs: { lib = lib pkgs; inherit pkgs; })
         nixpkgs.outputs.legacyPackages;
+      importFromGitHub = {owner, repo, rev, hash ? ""}: {
+        packages = builtins.mapAttrs
+          (system: pkgs: decorate-package pkgs
+            (import (pkgs.fetchFromGitHub { inherit owner repo rev hash; }){ idrx = self; }))
+          nixpkgs.outputs.legacyPackages;
+      };
+      importFromSrc = {src, ipkgName, idrisLibraries ? [], version ? "", buildInputs ? [], runtimeInputs ? []}: {
+        packages = builtins.mapAttrs
+          (system: pkgs: decorate-package pkgs
+            (pkgs.idris2Packages.buildIdris {
+              inherit src ipkgName idrisLibraries version;
+              nativeBuildInputs = buildInputs;
+              buildInputs = runtimeInputs;
+            } // {
+              inherit buildInputs runtimeInputs ipkgName idrisLibraries version;
+            })
+          )
+          nixpkgs.outputs.legacyPackages;
+      };
     };
 }
